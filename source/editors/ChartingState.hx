3c74726edacf1c0ffce98e36ecc9238c5f555540
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-14 21:00:27 -0600

    Added parry note

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -60,7 +60,8 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'GF Sing',
-		'No Animation'
+		'No Animation',
+		'Parry Note'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
