9d18c91729986991727f30296dd4e304ff035414
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-15 07:42:58 -0600

    :D added sans things

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -61,7 +61,9 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation',
-		'Parry Note'
+		'Parry Note',
+		'SansBlue',
+		'SansOrange'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
