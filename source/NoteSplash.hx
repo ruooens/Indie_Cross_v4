9469dc39d6acd6083c521eb264bfce42cfc6d4b5
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-16 17:22:23 -0600

    fixed note splashes

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -13,7 +13,7 @@ class NoteSplash extends FlxSprite
 	public function new(x:Float = 0, y:Float = 0, ?note:Int = 0) {
 		super(x, y);
 
-		var skin:String = 'ICsplashes';
+		var skin:String = 'noteSplashes';
 		if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) skin = PlayState.SONG.splashSkin;
 
 		loadAnims(skin);
@@ -30,7 +30,7 @@ class NoteSplash extends FlxSprite
 		alpha = 0.6;
 
 		if(texture == null) {
-			texture = 'ICsplashes';
+			texture = 'noteSplashes';
 			if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) texture = PlayState.SONG.splashSkin;
 		}
 
@@ -50,10 +50,10 @@ class NoteSplash extends FlxSprite
 	function loadAnims(skin:String) {
 		frames = Paths.getSparrowAtlas(skin);
 		for (i in 1...3) {
-			animation.addByPrefix("note1-" + i, "BlueC instance " + i, 24, false);
-			animation.addByPrefix("note2-" + i, "GreenC instance " + i, 24, false);
-			animation.addByPrefix("note0-" + i, "PurpC instance " + i, 24, false);
-			animation.addByPrefix("note3-" + i, "RedC instance " + i, 24, false);
+			animation.addByPrefix("note1-" + i, "note splash blue " + i, 24, false);
+			animation.addByPrefix("note2-" + i, "note splash green " + i, 24, false);
+			animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
+			animation.addByPrefix("note3-" + i, "note splash red " + i, 24, false);
 		}
 	}
 
