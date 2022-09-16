e767ebc22919b7735c541649595574325ae558ee
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-09-15 21:11:51 -0400

    remove this

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -127,11 +127,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.updateHitbox();
 		}
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, name + psychEngineVersion, 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + version, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, name + version, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
