de16614fca6a2202f751e15ee78b1d3505017886
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-09-17 17:42:50 -0400

    Update StartupVideo.hx

--- source/StartupVideo.hx
+++ source/StartupVideo.hx
@@ -7,20 +7,26 @@ import FlxVideo;
 #end
 
 class StartupVideo {
-    if(Assets.exists(Paths.video('intro'))) {
-        // inCutscene = true;
-        var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
-        bg.scrollFactor.set();
-        // bg.cameras = [camHUD];
-        add(bg);
+    public function new() {
+        startVideo();
+    }
 
-        (new FlxVideo(Paths.video('intro'))).finishCallback = function() {
-            remove(bg);
+    function startVideo() {
+        if(Assets.exists(Paths.video('intro'))) {
+            // inCutscene = true;
+            var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
+            bg.scrollFactor.set();
+            // bg.cameras = [camHUD];
+            add(bg);
+
+            (new FlxVideo(Paths.video('intro'))).finishCallback = function() {
+                remove(bg);
+                FlxG.switchState(new TitleState());
+            }
+            return;
+        } else {
+            FlxG.log.warn('Couldnt find video file: ' + fileName);
             FlxG.switchState(new TitleState());
         }
-        return;
-    } else {
-        FlxG.log.warn('Couldnt find video file: ' + fileName);
-        FlxG.switchState(new TitleState());
     }
 }
