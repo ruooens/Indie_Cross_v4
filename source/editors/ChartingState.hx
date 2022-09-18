e9ca7b1011633b967285c84a878a9c3f5a5ae1c6
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-17 21:33:44 -0600

    added bendy shit

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -60,7 +60,9 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'Parry Note',
 		'SansBlue',
-		'SansOrange'
+		'SansOrange',
+		'InkNote',
+		'ShadowNote'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
